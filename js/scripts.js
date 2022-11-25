b3c3c8f23784b4c967536f4e54907393bad6014a
IWBerends <isaac.berends@gmail.com>
2022-11-25 13:39:00 -0600

    2.3 Site Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log ("Hello everybody")
+function menuToggle () {
+  var x = document.getElementById('mynavtoggle')
+  // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
