d526d08cdfdaaed5b6b84d9de2c2265b13627779
IWBerends <isaac.berends@gmail.com>
2022-12-02 09:40:53 -0600

    update 12_2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function menuToggle () {
-  var x = document.getElementById('mynavtoggle')
-  // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive'
-  } else {
-    x.className = 'navtoggle'
+    var x = document.getElementById('mynavtoggle')
+    // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive'
+    } else {
+      x.className = 'navtoggle'
+    }
   }
-}
